3638a9381214318d2bc3267eabc7e0040672cfb4
rr210 <1577791638@qq.com>
2021-10-24 21:42:19 +0800

    update

--- src/assets/config/index.js
+++ src/assets/config/index.js
@@ -4,22 +4,22 @@
  * @Date: 2021-10-21 21:35:38
  * @Url: https://u.mr90.top
  * @github: https://github.com/rr210
- * @LastEditTime: 2021-10-23 17:45:39
+ * @LastEditTime: 2021-10-23 20:08:42
  * @LastEditors: Harry
  */
 // 模拟链接数据库
-// const urlLink = {
-//   CLASS_LINK: 'json/class_json/class.json', // 班级地址
-//   SUBMIT_POST: "http://localhost:3002/api", // 表单POST地址http://localhost:3002/api/
-//   TITLE_LINK: "json/title_json/title.json", // 标题地址
-//   TEACHER_LINK: "json/data_json/"
-// }
-// 正式版
-let url = ''
 const urlLink = {
-  CLASS_LINK: `${url}json/class_json/class.json`, // 班级地址
-  SUBMIT_POST: "/", // 表单POST地址http://localhost:3002/api/
-  TITLE_LINK: `${url}json/title_json/title.json`, // 标题地址
-  TEACHER_LINK: `${url}json/data_json/`
+  CLASS_LINK: 'json/class_json/class.json', // 班级地址
+  SUBMIT_POST: "http://localhost:3002/api", // 表单POST地址http://localhost:3002/api/
+  TITLE_LINK: "json/title_json/title.json", // 标题地址
+  TEACHER_LINK: "json/data_json/"
 }
+// 正式版
+// let url = '/static'
+// const urlLink = {
+//   CLASS_LINK: `${url}json/class_json/class.json`, // 班级地址
+//   SUBMIT_POST: "/", // 表单POST地址http://localhost:3002/api/
+//   TITLE_LINK: `${url}json/title_json/title.json`, // 标题地址
+//   TEACHER_LINK: `${url}json/data_json/`
+// }
 export default urlLink
