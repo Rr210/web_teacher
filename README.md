d39762e3f1988fec22832fa602d5eb48619c8b91
rr210 <1577791638@qq.com>
2021-10-26 14:16:13 +0800

    doc:supply documents

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
-# hello-world
+# 高校教师评测统计表
+
+## 实例图
+
+<table>
+<tr>
+<td><img src="https://cdn.jsdelivr.net/gh/Rr210/image@master/hexo/api/20211026140534.png"/></td>
+<td><img src="https://cdn.jsdelivr.net/gh/Rr210/image@master/hexo/api/20211026141016.png"/></td>
+<td><img src="https://cdn.jsdelivr.net/gh/Rr210/image@master/hexo/api/20211026141136.png"/></td>
+<tr>
+</table>
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/assets/config/index.js
+++ src/assets/config/index.js
@@ -4,22 +4,22 @@
  * @Date: 2021-10-21 21:35:38
  * @Url: https://u.mr90.top
  * @github: https://github.com/rr210
- * @LastEditTime: 2021-10-26 10:58:44
+ * @LastEditTime: 2021-10-26 11:03:30
  * @LastEditors: Harry
  */
 // 模拟链接数据库
-const urlLink = {
-  CLASS_LINK: 'json/class_json/class.json', // 班级地址
-  SUBMIT_POST: "http://localhost:3002/api", // 表单POST地址http://localhost:3002/api/
-  TITLE_LINK: "json/title_json/title.json", // 标题地址
-  TEACHER_LINK: "json/data_json/"
-}
-// 正式版
-// let url = 'static/'
 // const urlLink = {
-//   CLASS_LINK: `${url}json/class_json/class.json`, // 班级地址
-//   SUBMIT_POST: "/", // 表单POST地址http://localhost:3002/api/
-//   TITLE_LINK: `${url}json/title_json/title.json`, // 标题地址
-//   TEACHER_LINK: `${url}json/data_json/`
+//   CLASS_LINK: 'json/class_json/class.json', // 班级地址
+//   SUBMIT_POST: "http://localhost:3002/api", // 表单POST地址http://localhost:3002/api/
+//   TITLE_LINK: "json/title_json/title.json", // 标题地址
+//   TEACHER_LINK: "json/data_json/"
 // }
+// 正式版
+let url = 'static/'
+const urlLink = {
+  CLASS_LINK: `${url}json/class_json/class.json`, // 班级地址
+  SUBMIT_POST: "/", // 表单POST地址http://localhost:3002/api/
+  TITLE_LINK: `${url}json/title_json/title.json`, // 标题地址
+  TEACHER_LINK: `${url}json/data_json/`
+}
 export default urlLink
