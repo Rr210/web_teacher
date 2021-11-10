93bbeafc844cb1f9458ed5f26b44ad28b9e74b16
rr210 <1577791638@qq.com>
2021-11-10 18:14:01 +0800

    feat:关闭webpack 源代码显示

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,13 @@
  * @Date: 2021-10-20 21:46:45
  * @Url: https://u.mr90.top
  * @github: https://github.com/rr210
- * @LastEditTime: 2021-10-30 22:02:36
+ * @LastEditTime: 2021-11-10 12:07:58
  * @LastEditors: Harry
  */
 module.exports = {
   // ...
   publicPath: './',
+  productionSourceMap: false,
   // vue.config.js
   chainWebpack: config => {
     config
